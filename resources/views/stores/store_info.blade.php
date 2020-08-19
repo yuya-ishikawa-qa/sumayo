1aef35fb4af255155d9858c73ba4cd809d001e9a
masanori0117 <masanori.takahashi.0117@gmail.com>
2020-08-19 12:03:05 +0000

    googleMap マーカー表示機能追加

--- resources/views/stores/store_info.blade.php
+++ resources/views/stores/store_info.blade.php
@@ -51,7 +51,89 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     
     <!-- Google Map API script    -->
-    <script> 
+    <script>
+function initMap() {
+
+    var address = document.getElementById('address').textContent; 
+    var contentString = document.getElementById('address').textContent;
+
+    var addresses = [
+        address,
+    ];
+
+    var contentString = '<h1>{{ $store->name }}</h1>'+'<p>{{ $store->comment }}</p>';
+
+    var latlng = []; //緯度経度の値をセット
+    var marker = []; //マーカーの位置情報をセット
+    var myLatLng; //地図の中心点をセット用
+    var geocoder;
+    geocoder = new google.maps.Geocoder();
+
+    var map = new google.maps.Map(document.getElementById('map'));//地図を作成する
+
+    geo(aftergeo);
+
+    function geo(callback){
+        var cRef = addresses.length;
+        for (var i = 0; i < addresses.length; i++) {
+            (function (i) { 
+                geocoder.geocode({'address': addresses[i]}, 
+                    function(results, status) { // 結果
+                        if (status === google.maps.GeocoderStatus.OK) { // ステータスがOKの場合
+                            latlng[i]=results[0].geometry.location;// マーカーを立てる位置をセット
+                            marker[i] = new google.maps.Marker({
+                                position: results[0].geometry.location, // マーカーを立てる位置を指定
+                                map: map // マーカーを立てる地図を指定
+                            });
+                        } else { // 失敗した場合
+                        }//if文の終了。ifは文なので;はいらない
+                        if (--cRef <= 0) {
+                            callback();//全て取得できたらaftergeo実行
+                        }
+                    }//function(results, status)の終了
+                );//geocoder.geocodeの終了
+            }) (i);
+        }//for文の終了
+    }//function geo終了
+
+    function aftergeo(){
+        myLatLng = latlng[0];//最初の住所を地図の中心点に設定
+        var opt = {
+            center: myLatLng, // 地図の中心を指定
+            zoom: 16 // 地図のズームを指定
+        };//地図作成のオプションのうちcenterとzoomは必須
+        map.setOptions(opt);//オプションをmapにセット
+
+    }//function aftergeo終了
+
+    var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        marker.addListener('click', function() {
+        infowindow.open(map, marker);
+        });
+
+
+    
+  
+
+};//function initMap終了
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <script> 
       function initMap() {
         //地図を表示する領域の div 要素のオブジェクトを変数に代入
         var target = document.getElementById('map');  
@@ -95,8 +177,8 @@
           }
         });
       }
-    </script> 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZKrkayq5cqxe3tx3NqyoaDmWuW7YMkRg&callback=initMap" async defer></script>
+    </script>  -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCI3vmKszpyZPX9TfPekAF5DyXjMmbbYBU&callback=initMap" async defer></script>
 
 
     <div class="row justify-content-center mb-5">
